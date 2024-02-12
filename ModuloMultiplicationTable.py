79abcdba1c3475356a0290effd9b3fa1a68eba4e
WherBanan <121439318+WherBanan@users.noreply.github.com>
2024-02-12 22:12:18 +0200

    Add coloring and more grid size options

--- ModuloMultiplicationTable.py
+++ ModuloMultiplicationTable.py
@@ -1,13 +1,20 @@
+import random
 from tkinter import *
+
 root = Tk( )
 # Modulo
-Z = 4
+Z = 6
 
 # Grid Size
-S = 8
+S = 12
+height = 1
+widht = 2
 
-def GridMaker(x:str, r:int, c:int, colour:str): Button(root, text=str(x), fg='black', bg=colour, borderwidth=2, height= 3, width= 6 ).grid(row=r,column=c)
+def GridMaker(x:str, r:int, c:int, colour): Button(root, text=str(x), fg='black', bg=colour, borderwidth=2, height=height, width=widht).grid(row=r,column=c)
+def RandomColor(): return '#{0:02x}{1:02x}{2:02x}'.format(*[random.randint(150, 255) for _ in range(3)])
 
+
+colors = [RandomColor() for _ in range(Z)]
 
 for r in range(S):
     for c in range(S):        
@@ -19,5 +26,5 @@ for r in range(S):
         elif c == 0:
             GridMaker(r, r, c, "grey50")
         else:
-            GridMaker((r*c) % Z, r, c, "white")
+            GridMaker((r*c) % Z, r, c, str(colors[((r*c)%Z)]))
 root.mainloop()
