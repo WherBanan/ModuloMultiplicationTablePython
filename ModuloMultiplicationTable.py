cc574cc186e518260e62b41e6762486e8ef64a06
WherBanan <121439318+WherBanan@users.noreply.github.com>
2024-02-12 21:40:25 +0200

    Make grid size configurable independent to modulo

--- ModuloMultiplicationTable.py
+++ ModuloMultiplicationTable.py
@@ -3,18 +3,21 @@ root = Tk( )
 # Modulo
 Z = 4
 
+# Grid Size
+S = 8
+
 def GridMaker(x:str, r:int, c:int, colour:str): Button(root, text=str(x), fg='black', bg=colour, borderwidth=2, height= 3, width= 6 ).grid(row=r,column=c)
 
 
-for r in range(1, Z+1):
-    for c in range(1, Z+1):        
-        if r == 1:
-            if c == 1:
+for r in range(S):
+    for c in range(S):        
+        if r == 0:
+            if c == 0:
                 GridMaker("", r, c, "grey50")
             else:
-                GridMaker(c-1, r, c, "grey50")
-        elif c == 1:
-            GridMaker(r-1, r, c, "grey50")
+                GridMaker(c, r, c, "grey50")
+        elif c == 0:
+            GridMaker(r, r, c, "grey50")
         else:
-            GridMaker(((r-1)*(c-1)) % Z, r, c, "white")
+            GridMaker((r*c) % Z, r, c, "white")
 root.mainloop()
